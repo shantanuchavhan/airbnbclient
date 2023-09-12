65cbbe125cc26c098a59a1067998efea817d045e
shantanu <shantanuchavhan10@gmail.com>
2023-09-12 15:31:46 +0530

    commit

--- src/components/Become-a-Host/Footer.jsx
+++ src/components/Become-a-Host/Footer.jsx
@@ -1,152 +1,91 @@
 import React, { useState, useEffect } from 'react';
-
 import { useLocation, useNavigate, Link } from 'react-router-dom';
-import '../../styles/BecomeAHost/BecomeAHostPages.css';
 import { connect } from 'react-redux';
-import setCurrentUserListings from '../../Redux/Actions/setCurrentUserListings';
+import { createOrUpdateListing } from '../../Redux/Actions/listingActions'; // Replace with your actual action
 
-const Footer = ({ listing,userName ,setCurrentUserListings,currentUserListings}) => {
+const Footer = ({ listing, userName, createOrUpdateListing }) => {
   const [lastPage, setLastPage] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
   const currentPathName = location.pathname.split('/').pop();
 
   useEffect(() => {
-    if (currentPathName === 'publish-celebration') {
-      setLastPage(true);
-    } else {
-      setLastPage(false);
-    }
+    setLastPage(currentPathName === 'publish-celebration');
   }, [currentPathName]);
 
-
   const PageList = [
-    "overview", "about-your-place", "structure", "privacy-type", "location",
-    "floor-plan", "stand-out", "amenities", "photos", "title", "description",
-    "finish-setup", "price", "discount", "publish-celebration"
+    'overview', 'about-your-place', 'structure', 'privacy-type', 'location',
+    'floor-plan', 'stand-out', 'amenities', 'photos', 'title', 'description',
+    'finish-setup', 'price', 'discount', 'publish-celebration'
   ];
 
   const currentIndex = PageList.indexOf(currentPathName);
 
-  const Next = () => {
+  const nextPath = () => {
     if (currentIndex < PageList.length - 1) {
-      const nextPath = '/Become-a-Host/' + PageList[currentIndex + 1];
-      navigate(nextPath);
-    } else {
-      setLastPage(true);
+      return `/Become-a-Host/${PageList[currentIndex + 1]}`;
     }
+    return null;
   };
 
-  const Back = () => {
+  const goBack = () => {
     if (currentIndex > 0) {
-      const prevPath = '/Become-a-Host/' + PageList[currentIndex - 1];
+      const prevPath = `/Become-a-Host/${PageList[currentIndex - 1]}`;
       navigate(prevPath);
     }
   };
 
-  const userData=userName
-  // console.log(userData.userName,"userData.userName")
-  let formData=new FormData()
-  for (let i = 0; i < listing.photos.length; i++) {
-    console.log(listing.photos[i], "listingPhotos");
-    formData.append('photos',listing.photos[i])   
-  }
-  listing={...listing,ownerName:userData.userName}
- 
-  formData.append('listing', JSON.stringify(listing));
-  
-  
-  
-
-  
-  function CreateAListing() {
- if (listing.id !== null) {
-  const url = `https://airbnbcloneshantanu.onrender.com/api/listings/${listing.id}`;
-
-  fetch(url, {
-    method: 'PUT',
-    body: formData
-  })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
+  const handlePublish = async () => {
+    try {
+      const formData = new FormData();
+      for (const photo of listing.photos) {
+        formData.append('photos', photo);
       }
-      return response.json();
-    })
-    .then(data => {
-      alert('Listing updated successfully');
-      console.log(data, "datalog");
-      setCurrentUserListings([...currentUserListings, ...data.listings]);
-    })
-    .catch(error => {
+      formData.append('listing', JSON.stringify({ ...listing, ownerName: userName.userName }));
+      
+      const response = await createOrUpdateListing(listing.id, formData);
+      if (response) {
+        alert('Listing updated successfully');
+      } else {
+        alert('Listing created successfully');
+      }
+    } catch (error) {
       alert(`Error: ${error.message}`);
-      console.error('Fetch Error:', error);
-    });
-}else{
-    fetch('https://airbnbcloneshantanu.onrender.com/api/listing', {
-  method: 'POST',
-  body: formData
-})
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok',response.error);
     }
-    return response.json();
-  })
-  .then(data => {
-    alert('Listing created succesfully')
-    console.log(data,"datalog")
-    setCurrentUserListings([...currentUserListings,...data.listings])
-    // 
-    
-  })
-  .catch(error => {
-    alert(error)
-    console.error('Fetch Error:', error);
-  });
-
-  }
-    
-
-  
-  
-
-
-  }
-
-  
+  };
 
   return (
     <div className="footer">
       <div>
-        <button className='BackFooterBtn' onClick={Back}>Go Back</button>
+        <button className="BackFooterBtn" onClick={goBack}>
+          Go Back
+        </button>
       </div>
-      {!lastPage ? (
+      {!lastPage && (
         <div>
-          <button className='nextFooterBtn' onClick={Next}>Next</button>
+          <button className="nextFooterBtn" onClick={() => navigate(nextPath())}>
+            Next
+          </button>
         </div>
-      ) : (
+      )}
+      {lastPage && (
         <div>
-          <Link className='Links' onClick={CreateAListing} to='/hosting'><h3 className="PublishListing">Publish Listing</h3></Link>
+          <Link className="Links" onClick={handlePublish} to="/hosting">
+            <h3 className="PublishListing">Publish Listing</h3>
+          </Link>
         </div>
       )}
     </div>
   );
 };
 
-const mapStateToProps = (state) => {
-  return {
-    listing: state.ListingReducer,
-    userName:state.userName,
-    currentUserListings:state.currentUserListingsReducer.currentUserListings
-  };
-};
+const mapStateToProps = (state) => ({
+  listing: state.ListingReducer,
+  userName: state.userName,
+});
 
 const mapDispatchToProps = {
- 
- setCurrentUserListings
-  
-  
+  createOrUpdateListing, // Replace with your actual action
 };
 
-export default connect(mapStateToProps,mapDispatchToProps)(Footer);
+export default connect(mapStateToProps, mapDispatchToProps)(Footer);
