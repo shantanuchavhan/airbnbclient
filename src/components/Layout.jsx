29922bf266573ad65cbcebed996518088a319c34
shantanu <shantanuchavhan10@gmail.com>
2023-09-15 12:20:08 +0530

    commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -25,7 +25,7 @@ const Layout = ({children}) => {
         <Outlet>{children}</Outlet>
       </div>
       
-      <div className="largeHeader">
+      <div className="largeFooter">
         <Footer />
       </div>
       
