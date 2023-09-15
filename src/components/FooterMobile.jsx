08205b6b1ff89a150baab04e83a196f91e52ac40
shantanu <shantanuchavhan10@gmail.com>
2023-09-15 20:54:17 +0530

    commit

--- src/components/FooterMobile.jsx
+++ src/components/FooterMobile.jsx
@@ -1,99 +1,116 @@
-import React from 'react'
-import '../styles/Footer.css'
-// import { useState } from 'react'
+import React from 'react';
+import '../styles/Footer.css';
 import { connect } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-const FooterMobile = ({userName}) => {
-    const navigate=useNavigate()
-    console.log(userName)
-    const logout = [
-        {
-            name:"Explore",
-            svg:<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-            </svg>
-        },{
-            name:"Wishlists",
-            svg:<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+
+const FooterMobile = ({ userName }) => {
+  const navigate = useNavigate();
+
+  const navigateTo = (section) => {
+    if (section === "Login") {
+      navigate('/Login');
+    } else if (section === "Explore") {
+      navigate('/');
+    }
+  };
+
+  const logoutSections = [
+    {
+      name: "Explore",
+      svg: (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
         </svg>
-
-        },{
-            name:"Login",
-            svg:<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                </svg>
-        }]
-    const login = [
-        {
-            name:"Explore",
-            svg:<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-            </svg>
-        },{
-            name:"Wishlists",
-            svg:<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+      ),
+    },
+    {
+      name: "Wishlists",
+      svg: (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
         </svg>
+      ),
+    },
+    {
+      name: "Login",
+      svg: (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+        </svg>
+      ),
+    },
+  ];
 
-        },{
-            name:"Trips",
-            svg:<img src="https://a0.muscache.com/airbnb/static/icons/android-icon-192x192-c0465f9f0380893768972a31a614b670.png" alt="" />
-        }
-        ,{
-            name:"Inbox",
-            svg:<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-          </svg>
-          
-        },{
-            name:"Profile",
-            svg:<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                </svg>
-        }]
-      const navigateTo=(section)=>{
-        if(section==="Login"){
-            navigate('/Login')
-        }else if(section==="Explore"){
-            navigate('/')
-        }
-      } 
-        
-
+  const loginSections = [
+    {
+      name: "Explore",
+      svg: (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+        </svg>
+      ),
+    },
+    {
+      name: "Wishlists",
+      svg: (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+        </svg>
+      ),
+    },
+    {
+      name: "Trips",
+      svg: (
+        <img src="https://a0.muscache.com/airbnb/static/icons/android-icon-192x192-c0465f9f0380893768972a31a614b670.png" alt="" />
+      ),
+    },
+    {
+      name: "Inbox",
+      svg: (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+        </svg>
+      ),
+    },
+    {
+      name: "Profile",
+      svg: (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+        </svg>
+      ),
+    },
+  ];
 
   return (
     <div className="FooterMobile">
-      {
-        userName.userName!=="" ? login.map((value)=>(
-            <div className="FooterMobile__Section">
-                
-                <div className={value.name==="Trips" ? "airbnblogo":""}>
-            {value.svg}
+      {userName.userName !== "" ? (
+        loginSections.map((value) => (
+          <div className="FooterMobile__Section" key={value.name}>
+            <div onClick={() => navigateTo(value.name)} className={value.name === "Trips" ? "airbnblogo" : ""}>
+              {value.svg}
             </div>
-            <h3>{value.name}</h3>
-       </div>
-        )):
-        logout.map((value)=>(
-            <div className="FooterMobile__Section">
-            <div onClick={navigateTo(value.name)} className={value.name==="Trips" ? "airbnblogo":""}>
-            {value.svg}
-            </div>
-            <h3 onClick={navigateTo(value.name)}>{value.name}</h3>
-        </div>
-
+            <h3 onClick={() => navigateTo(value.name)}>{value.name}</h3>
+          </div>
         ))
-      }
-      
+      ) : (
+        logoutSections.map((value) => (
+          <div className="FooterMobile__Section" key={value.name}>
+            <div onClick={() => navigateTo(value.name)} className={value.name === "Trips" ? "airbnblogo" : ""}>
+              {value.svg}
+            </div>
+            <h3 onClick={() => navigateTo(value.name)}>{value.name}</h3>
+          </div>
+        ))
+      )}
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = (state) => {
-    return {
-      userName: state.userName, 
-     
-    };
+  return {
+    userName: state.userName,
   };
+};
 
-export default connect( mapStateToProps) (FooterMobile)
+export default connect(mapStateToProps)(FooterMobile);
