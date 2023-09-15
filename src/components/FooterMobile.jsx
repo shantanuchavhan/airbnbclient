42b096ab689d8d3a1639c4bf4f08d3ccbfa38ea6
shantanu <shantanuchavhan10@gmail.com>
2023-09-15 14:40:33 +0530

    commit

---
+++ src/components/FooterMobile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterMobile = () => {
+  return (
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default FooterMobile
