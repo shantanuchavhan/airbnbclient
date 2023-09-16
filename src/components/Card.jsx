4509e700b5e99b46bcb88a1675a11ae650dfbf32
shantanu <shantanuchavhan10@gmail.com>
2023-09-16 21:53:10 +0530

    commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -21,7 +21,7 @@ const Card = ({ key,userName, roomData,allWishList }) => {
   // image = "https://airbnbcloneshantanu.onrender.com/" + image;
 
   const setProduct = () => {
-    console.log(navigateToLogin)
+   
     // if(navigateToLogin===true){
       
     //   navigate('/Login')
