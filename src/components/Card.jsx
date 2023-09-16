ee4f59d8eade3b86885b9b627fd5c6275e5adc45
shantanu <shantanuchavhan10@gmail.com>
2023-09-16 18:02:35 +0530

    commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,7 @@ import Cookies from 'js-cookie';
 
 
 const Card = ({ key,userName, roomData,allWishList }) => {
-  const []
+  
   const [isLoading, setIsLoading] = useState(true);
   const navigate=useNavigate()
   useEffect(() => {
