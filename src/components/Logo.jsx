71718e19ae68a3a22b46aa39817524689e171fce
shantanu <shantanuchavhan10@gmail.com>
2023-09-20 13:40:13 +0530

    commit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,13 @@ import '../App.css'
 import logo from '../Images/Airbnb-logo.png'
 const Logo = () => {
   return (
-    <img src={logo} className="App-logo" alt="" />
+    <div>
+      <img src={logo} className="App-logo bigairbnblogo" alt="" />
+    <img className='smallAirbnblogo'  src="https://a0.muscache.com/airbnb/static/icons/android-icon-192x192-c0465f9f0380893768972a31a614b670.png" alt="" />
+
+    </div>
+    
+    
   )
 }
 
