4d2d1ed59ae6b9df9c786628d45b4de0b3b99e5b
shantanu <shantanuchavhan10@gmail.com>
2023-09-21 14:30:14 +0530

    commit

--- src/components/HeaderMobile.jsx
+++ src/components/HeaderMobile.jsx
@@ -9,8 +9,8 @@ const HeaderMobile = () => {
         
     }
   return (
-    <div onClick={SwitchSearchSection} className="HeaderMobile">
-        <div className="SearchSectionMobile flexA">
+    <div  className="HeaderMobile">
+        <div onClick={SwitchSearchSection} className="SearchSectionMobile flexA">
             <div className="flexA">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
