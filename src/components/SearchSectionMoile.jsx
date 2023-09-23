d43d6fdd14e135979ef7b12dc9ea1613c945c1a3
shantanu <shantanuchavhan10@gmail.com>
2023-09-23 13:57:01 +0530

    commit

--- src/components/SearchSectionMoile.jsx
+++ src/components/SearchSectionMoile.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 import { useState } from 'react'
 import '../styles/SearchSection.css'
 import PinkButton from './PinkButton'
+import setRooms from '../Redux/Actions/setRooms'
 
-
-const SearchSectionMoile = ({SwitchSearchSection}) => {
+const SearchSectionMoile = ({SwitchSearchSection , setRooms}) => {
+    const [location,setLocation]=useState("")
+    const [checkInDate,setCheckInDate]=useState("")
+    const [checkOutDate,setCheckOutDate]=useState("")
     function SearchIt(e) {
         e.preventDefault();
         
@@ -13,7 +16,7 @@ const SearchSectionMoile = ({SwitchSearchSection}) => {
           location: location.toLowerCase(),
           checkInDate: checkInDate,
           checkOutDate: checkOutDate,
-          totalGuestCount: totalGuestCount
+         
         };
       
         fetch("http://localhost:5000/search", {
@@ -36,15 +39,8 @@ const SearchSectionMoile = ({SwitchSearchSection}) => {
       }
       
     
-        function WhoSection(){
-          GuestToggle()
-          WhoActive()
-    
-        }
-    
-        const [location,setLocation]=useState("")
-        const [checkInDate,setCheckInDate]=useState("")
-        const [checkOutDate,setCheckOutDate]=useState("")
+        
+        
     
         
 
@@ -65,18 +61,17 @@ const SearchSectionMoile = ({SwitchSearchSection}) => {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
-            <input type="text" placeholder="Search Destinations"/>
+            <input onChange={(e)=>setLocation(e.target.value)}  type="text" placeholder='Search Destinations' />
         </div>
         </div>
         <div className="SearchSectionMoile-Box displayFlex justifycontainBtn">
-            <h3 className="gray">From</h3>
-            <input type="date"  />
+            <h3 className="gray">Check in</h3>
+            <input type="Date" onChange={(e)=>setCheckInDate(e.target.value)} placeholder='Add Date' />
            
         </div>
         <div className="SearchSectionMoile-Box displayFlex justifycontainBtn">
-            <h3 className="gray">To</h3>
-            <input type="date" />
-         
+            <h3 className="gray">Check out</h3>
+            <input type="Date" onChange={(e)=>{setCheckOutDate( e.target.value)}}  placeholder='Add Date' />        
         </div>
        
         </div>
@@ -91,4 +86,13 @@ const SearchSectionMoile = ({SwitchSearchSection}) => {
   )
 }
 
-export default SearchSectionMoile
+
+  
+  const mapDispatchToProps = {
+    
+    setRooms
+  };
+  
+  export default connect( mapDispatchToProps)(SearchSectionMoile);
+
+
