da9ce78efc6d673a18ac87ed80f268d330ebf9f2
shantanu <shantanuchavhan10@gmail.com>
2023-10-05 14:14:07 +0530

    commit

--- src/Pages/ProductPageMoile.jsx
+++ src/Pages/ProductPageMoile.jsx
@@ -15,7 +15,7 @@ const ProductPageMobile = ({currentProduct,userName,SetIsBooking,SetHeaderFooter
     SetHeaderFooter(false)
     console.log(isFooterHeader,"isFooterHeader")
 
-  },[SetHeaderFooter])
+  },[SSetHeaderFooter,isFooterHeader])
   
   console.log(currentProduct,"currentProduct")
   const images = currentProduct.photos.map((photoUrl) => ({
