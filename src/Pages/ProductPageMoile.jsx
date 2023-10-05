a2a2d70d8a31a8827a8253c1cbf0208c899a3dfb
shantanu <shantanuchavhan10@gmail.com>
2023-10-05 11:02:16 +0530

    commit

---
+++ src/Pages/ProductPageMoile.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import SetIsBooking from '../Redux/Actions/SetIsBooking'
+
+import SimpleImageSlider from "react-simple-image-slider";
+
+
+const ProductPageMobile = ({currentProduct,userName,SetIsBooking}) => {
+  const sliderImages = [
+    {
+       url: "https://encryptedtbn0.gstatic.com/images?q=tbn:ANd9GcTdUmK6fRPfPQIMyOxUOjiTnR2VYizvkkWUxLhGoVJNobm5Id3fpRhsIGtPj3H06ujPb4&usqp=CAU",
+    },
+    {
+       url: "https://encryptedtbn0.gstatic.com/images?q=tbn:ANd9GcQduUpBWhSdgkWqqIrSmw5MMU697Xlx3SCjlB4eZSv0Q&s",
+    },
+    {
+       url: "https://encryptedtbn0.gstatic.com/images?q=tbn:ANd9GcTnzApyh1ZmbXLBUg_iFRio23hzRyAJfwBRfnVozXdEnu-NK4jFt2_gsYujKf-CbT6Cr_A&usqp=CAU",
+    },
+    {
+       url: "https://encryptedtbn0.gstatic.com/images?q=tbn:ANd9GcTDnfOIY9gjVyoT4ulMp55roiV5KefqaDByUc0HdD8p3tdpXuwxTaXjhLdyUFeQzZ2ZwE&usqp=CAU",
+    },
+    {
+       url: "https://encryptedtbn0.gstatic.com/images?q=tbn:ANd9GcR3IVlvufXIDZXxq0O8SVqwU2HeO6y7as0OXJl-YT55BA&s",
+    },
+    {
+       url: "https://encryptedtbn0.gstatic.com/images?q=tbn:ANd9GcQs_7aafRRY4vEbWz2wydowaogMmGI7mRVG6MQfZVtKDFXUGqt5iF-Mu0AYMQBEeznPkU&usqp=CAU",
+    },
+    {
+       url: "https://smartslider3.com/wpcontent/uploads/2019/01/photo_slideshow.jpg",
+    },
+ ];
+  return (
+    <div>
+      <div className="ProductPageMobile__header">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+        </svg>
+
+        <div>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
+          </svg>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+          </svg>
+        </div>
+      </div>
+      <div className="ProductPageMobile">
+      <SimpleImageSlider
+            width={500}
+            height={250}
+            images={sliderImages}
+            showNavs={true}
+         />
+        
+      </div> 
+   </div>
+  )
+}
+
+
+
+const mapStateToProps = (state) => {
+  return {
+    currentProduct: state.CurrentProductReducer.currentProduct,
+    userName:state.userName.userName
+  };
+};
+
+const mapDispatchToProps = {
+  SetIsBooking
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductPageMobile); 
+
