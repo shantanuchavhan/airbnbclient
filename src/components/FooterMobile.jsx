3f42ac78b76b6d49310b88fefa6e1b9a44b45729
shantanu <shantanuchavhan10@gmail.com>
2023-10-05 13:48:11 +0530

    commit

--- src/components/FooterMobile.jsx
+++ src/components/FooterMobile.jsx
@@ -37,7 +37,7 @@ const FooterMobile = ({ userName, isFooterHeader }) => {
     }
   };
 
-  const sections = [
+  const logoutSections = [
     {
       name: "Explore",
       svg: (
@@ -56,6 +56,47 @@ const FooterMobile = ({ userName, isFooterHeader }) => {
     },
     {
       name: "Login",
+      svg: (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+        </svg>
+      ),
+    },
+  ];
+
+  const loginSections = [
+    {
+      name: "Explore",
+      svg: (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+        </svg>
+      ),
+    },
+    {
+      name: "Wishlists",
+      svg: (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+        </svg>
+      ),
+    },
+    {
+      name: "Trips",
+      svg: (
+        <img src="https://a0.muscache.com/airbnb/static/icons/android-icon-192x192-c0465f9f0380893768972a31a614b670.png" alt="" />
+      ),
+    },
+    {
+      name: "Inbox",
+      svg: (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+        </svg>
+      ),
+    },
+    {
+      name: "Profile",
       svg: (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
@@ -94,7 +135,7 @@ const FooterMobile = ({ userName, isFooterHeader }) => {
 const mapStateToProps = (state) => {
   return {
     userName: state.userName,
-    isFooterHeader: FooterHeaderNoneReducer.isFooterHeader
+    isFooterHeader: state.FooterHeaderNoneReducer.isFooterHeader
   };
 };
 
