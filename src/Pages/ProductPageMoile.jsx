c1c900dd60979cfcbfd4489d58eb662c784ad0df
shantanu <shantanuchavhan10@gmail.com>
2023-10-05 13:08:46 +0530

    commit

--- src/Pages/ProductPageMoile.jsx
+++ src/Pages/ProductPageMoile.jsx
@@ -20,7 +20,7 @@ const ProductPageMobile = ({currentProduct,userName,SetIsBooking}) => {
   
   console.log(images,"images")
   return (
-    <div>
+    <div className="ProductPageMobile">
       <div className="ProductPageMobile__header">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
