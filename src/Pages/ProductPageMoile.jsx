9f8ac3c75d1f40516ca38281100ff0fcfdc5c21d
shantanu <shantanuchavhan10@gmail.com>
2023-10-05 16:04:43 +0530

    commit

--- src/Pages/ProductPageMoile.jsx
+++ src/Pages/ProductPageMoile.jsx
@@ -42,6 +42,8 @@ const ProductPageMobile = ({currentProduct,userName,SetIsBooking,SetHeaderFooter
           </svg>
         </div>
       </div>
+
+
       <div className="ProductPageMobile_photos">
         <Carousel className='ProductPageMobile_photos_Carousel'>
           {images.map((image, index) => (
@@ -51,6 +53,31 @@ const ProductPageMobile = ({currentProduct,userName,SetIsBooking,SetHeaderFooter
           ))}
       </Carousel>
       </div> 
+
+
+      <div className="details-1">
+            <h1>{currentProduct?.title||""}</h1>
+            <div className="details1-link">
+                <div>
+                <h4 onClick={handleViewReviewsClick} className='Rating ReservationBox__section1__averageRating' >{currentProduct && currentProduct.averageRating !== undefined ? currentProduct.averageRating.toFixed(1) : 0}
+
+            <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3">
+                <path stroke-linecap="round" stroke-linejoin="round" className='fillRatingSvg' d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+            </svg>
+            </h4>
+            <div>
+                <h1> </h1></div>
+                <h5>Superhost</h5>
+                <div><h1> </h1></div>
+                <h5 className="location">{currentProduct.location}</h5>
+                </div>
+                <div>
+                    <h5>Share</h5>
+                    <h5>Save</h5>
+                </div>
+            </div>
+        </div>
+
       <div className="ProductPageMobile_footer">
         <div className="ProductPageMobile_footer__Charges">
           <h3><h2>{currentProduct.price}</h2> night</h3>
