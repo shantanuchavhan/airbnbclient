ee2721964796333d6e9c671a2438d42e39a8a95d
shantanu <shantanuchavhan10@gmail.com>
2023-10-05 14:51:32 +0530

    commit

--- src/components/HeaderMobile.jsx
+++ src/components/HeaderMobile.jsx
@@ -15,7 +15,7 @@ const HeaderMobile = ({ isFooterHeader}) => {
   return (
     
         isFooterHeader ?
-        (<div  className="HeaderMobile">
+        (<div  className={isFooterHeader?".HeaderMobile":""}>
         <div onClick={SwitchSearchSection} className="SearchSectionMobile flexA">
             <div className="flexA">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
