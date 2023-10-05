f88659fdf46214b7281aba82897a0f749427481c
shantanu <shantanuchavhan10@gmail.com>
2023-10-05 16:31:16 +0530

    commit

--- src/Pages/ProductPageMoile.jsx
+++ src/Pages/ProductPageMoile.jsx
@@ -78,6 +78,36 @@ const ProductPageMobile = ({ currentProduct, userName, SetIsBooking, SetHeaderFo
         </div>
       </div>
 
+
+      <div>
+          <h2>Entire Place Hosted By {currentProduct.ownerName}</h2>
+          <div className='Features'>
+              <h5>{currentProduct.floorplan.guestCount||currentProduct.floorplan.guest} Guests</h5>
+              <div><h1> </h1></div> 
+              <h5>{currentProduct.floorplan.bedCount||currentProduct.floorplan.bed} Bedroom</h5>
+              <div><h1> </h1></div>
+              <h5>{currentProduct.floorplan.bedCount+1||currentProduct.floorplan.bed+1} Bathroom</h5>
+              <div><h1> </h1></div>
+              <h5>{currentProduct.floorplan.bathroomCount+1||currentProduct.floorplan.bathroom+1} Bathroom</h5>
+              <div><h1> </h1></div>
+          </div>
+          
+          </div> 
+          <img src={"https://airbnbcloneshantanu.onrender.com/"+ currentProduct.photos[0]} alt="hghj" className="Ownerimg" />
+          <div style={{display:'flex', gap:'24px',alignItems:'center' }}>
+              <h3>Rs.{currentProduct.price} </h3><span style={{color:'gray'}}>ninght</span>
+              <div className='Rating'>
+                  <h4 onClick={handleViewReviewsClick}  className='ReservationBox__section1__averageRating' >{currentProduct && currentProduct.averageRating !== undefined ? currentProduct.averageRating.toFixed(1) : 0}
+          <svg      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" className='fillRatingSvg' d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+          </svg>
+          </h4>
+          <h4 onClick={handleViewReviewsClick}  >{currentProduct.reviews.length} Reviews</h4>
+              </div>
+          </div>
+
+                
+
       <div className="ProductPageMobile_footer">
         <div className="ProductPageMobile_footer__Charges">
           <h3 className="displayFlex gap8">
