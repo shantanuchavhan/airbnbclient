7d2886f2b22eeaa9f3da89e8bfebdd1f9578bb2c
shantanu <shantanuchavhan10@gmail.com>
2023-10-06 01:15:41 +0530

    commit

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -75,19 +75,22 @@ const ProfilePage = ({ChangeUsername,userName }) => {
 
       </div>
       
-      <div className="ProfilePage-Section">
+      <div className="ProfilePage-Section HostingSection">
+
         <h2>Hosting</h2>
-        <div className="displayFlex alignItemsCenter justifycontainBtn border-bottom" >
-        <div className="displayFlex alignItemsCenter gap16">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
-        </svg>
-        <h3>Manage your listing</h3>
+
+        <div className="Hosting-link" >
+          <div className="displayFlex alignItemsCenter gap16">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
+          </svg>
+          <h3>Manage your listing</h3>
+          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+          </svg>
         </div>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-        </svg>
-        </div>
+
       </div>
 
       <Button onClickAction={logout} BtnName="Log Out" LinkTo="/" width="100%" />
