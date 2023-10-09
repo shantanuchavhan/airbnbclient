eecaa45f7dd7749d4a4faf478ef0a8898774fae1
shantanu <shantanuchavhan10@gmail.com>
2023-10-09 14:53:08 +0530

    commit

--- src/components/HeaderMobile.jsx
+++ src/components/HeaderMobile.jsx
@@ -19,7 +19,7 @@ const HeaderMobile = () => {
             </div>
             <div>
                 <h3>Anywhere</h3>
-                <h3 className="gray marginTop-10">Any week . Add guests</h3>
+                <h4 className="gray marginTop-10">Any week . Add guests</h4>
             </div>
         </div>
         <div className="filterMobile">
