35aac95c751fc11514f5da4333b5b5e272f866ab
shantanu <shantanuchavhan10@gmail.com>
2023-10-13 16:22:13 +0530

    commit

--- src/components/BookingComponent/Payment.jsx
+++ src/components/BookingComponent/Payment.jsx
@@ -83,8 +83,19 @@ const Payment = ({currentProduct, tripData,setPayment}) => {
                     <PinkButton BtnName="Login Required" link="/Login" />
                 </div>
             ):(
-                <div>
+                <div className="PaymentMobile_section" >
+                        <h3>Pay with</h3>
+                        <div className="PaymentMobile_paymentType">
+                            <div>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
+                                </svg>
+                                <h4>Credit or debit card</h4>
+                            </div>
+                            <a href="http://">Edit</a>
+                        </div>
 
+                    
                     
                 </div>
             ) }
